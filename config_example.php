69a6b61067fa66ada2a36d797b0b2355fa574026
dr4g0nsr <dragonmen@gmail.com>
2020-01-12 15:25:10 +0100

    Ignore and example

--- config_example.php
+++ config_example.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = [
-    'CHECK_PATH' => ['/var/www/html/mysite'],
+    'CHECK_PATH' => ['/var/www/html/mysite','/var/www/html/mysite2'],
     'SLACK_TOKEN' => 'xoxp-XXXXXXXX-XXXXXXXXXXXX-XXXXXXXXXXXXXX-XXXXXXXXXXXXXXX',
     'SLACK_CHANNEL' => '#mychannel',
 ];
